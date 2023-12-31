ae7f7e7d7dffbea738d04c6dcebf8f3acfef3913
d.baltabaev <d.baltabaev@tinkoff.ru>
2023-12-31 22:59:03 +0600

    OTV: init commit

---
+++ src/main/java/org/example/otvattendancebot/OtvAttendanceBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.otvattendancebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtvAttendanceBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtvAttendanceBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/otvattendancebot/OtvAttendanceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.otvattendancebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtvAttendanceBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
